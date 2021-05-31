59b226360b9f1278b6cd52a70966589d23564f4f
NumenMirus <michaelbastauni@gmail.com>
2021-05-31 23:56:58 +0200

    added graphic prototype

---
+++ graphic.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+root.title("DocSity Downloader")
+root.iconbitmap("/home/numen/Desktop/progetti/docsity-downloader/dd.ico")
+
+e = Entry(root, width=50, borderwidth=3)
+e.grid(row=0, column=0, columnspan=1)
+
+button_1 = Button(root, text="hack it!")
+
+root.mainloop()
